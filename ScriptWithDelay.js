b74c58294eabae7c1315207105a32ca42e379f67
Renuka <thotarenuka0612@gmail.com>
2025-07-10 23:23:53 -0400

    Waiting with Delays

---
+++ ScriptWithDelay.js
@@ -0,0 +1,19 @@
+function Furniture_to_mobile()
+{
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btChrome).Run("https://smartbearstore.smartbear.com/samples/TestComplete15/smartstore/");
+  //Clicks the 'Chrome_RenderWidgetHostHWND' object.
+  Aliases.browser.BrowserWindow.Chrome_RenderWidgetHostHWND.Click(464, 354);
+  //Simulates a left-button single click in a window or control as specified (relative position, shift keys).
+  Aliases.browser.pageShop.sectionContent.articleFurniture.textnodeFurniture.Click();
+  //Simulates a left-button single click in a window or control as specified (relative position, shift keys).
+  delay(5000);
+  Aliases.browser.pageShopFurniture.header.navBooks.WaitEle
+  Aliases.browser.pageShopFurniture.header.navBooks.textnodeMobilePhone.linkMobilePhone.Click();
+  //Simulates a left-button single click in a window or control as specified (relative position, shift keys).
+  delay(5000);
+  Aliases.browser.WaitChild(Aliases.browser.pageShopApple2.sectionContent.article9.linkShowDetailsForTheTablet.imageShowDetailsForTheTablet);
+  Aliases.browser.pageShopApple2.sectionContent.article9.linkShowDetailsForTheTablet.imageShowDetailsForTheTablet.Click();
+  //Closes the specified Window object.
+  Aliases.browser.BrowserWindow.Close();
+}
